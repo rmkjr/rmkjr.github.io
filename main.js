3baa97e69355d187202cea85f894f1899d6e3a2d
rmkjr <richardmkovachjr@gmail.com>
2015-08-07 00:06:22 -0400

    more

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
     "use strict";
     $('#name').click(function () {
+        $('#resume-content').fadeOut(300);
         $('.header')[0].removeAttribute('style');
     });
     $('#resume-link').click(function () {
         $('.header')[0].style.height = '5em';
+        $('#resume-content').show();
     });
 });
