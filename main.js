d122bedd3dfced071b7227b1a887ae5ad681bdb7
rmkjr <richardmkovachjr@gmail.com>
2015-08-07 00:33:12 -0400

    more

--- main.js
+++ main.js
@@ -8,4 +8,9 @@ $(document).ready(function () {
         $('.header')[0].style.height = '5em';
         $('#resume-content').show();
     });
+    $('#photo-link').click(function () {
+        $('body').fadeOut(1000, function () {
+            window.location.href = "https://www.flickr.com/photos/richardkovach";
+        });
+    });
 });
